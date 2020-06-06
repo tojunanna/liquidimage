353a277a485858574eb7fc0e8a9ddf1c26e10838
Toju Roberto Nanna <36927120+tojunanna@users.noreply.github.com>
2020-06-06 11:31:12 +0100

    Add files via upload
    initial commit

---
+++ Blended Hover_files/app.js
@@ -0,0 +1,7 @@
+new hoverEffect({
+    parent: document.querySelector(".distortion"),
+    intensity:0.2,
+    image1: "p.jpg",
+    image2: "ppp.jpg",
+    displacementImage: "heightMap.png"
+});
---
+++ app.js
@@ -0,0 +1,9 @@
+new hoverEffect({
+    parent: document.querySelector(".distortion"),
+    intensity:0.9,
+    image1: "p.jpg",
+    image2: "ppp.jpg",
+    speedIn:2,
+    speedOut: 5,
+    displacementImage: "heightMap.png"
+});
